880988630c177b952cbf01ca0f35ab553bd2df56
jorgeirish <jorgefdezavila@gmail.com>
2023-02-20 22:09:16 +0100

    Last Update

---
+++ Material_de_trabajo/Entregables/doc_python/def_barcos_player.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+# Tablero selección barcos 10x10 con agua 
+def colocar_barcos(barco):
+
+    tablero = np.full((10,10)," ")
+
+    eslora = barco
+    import random
+
+    while True:
+        # 'N' - 'S' - 'E' - 'O'
+        orient = random.choice(['N', 'S', 'E', 'O'])
+
+        # Posicion inicial del barco
+        current_pos = np.random.randint(10, size = 2)
+    
+        fila = current_pos[0]
+        col = current_pos[1]
+    
+        # Recogemos las 4 posiciones colindantes a current_pos
+        coors_posN = tablero[fila:fila - eslora:-1, col]
+        coors_posE = tablero[fila, col: col + eslora]
+        coors_posS = tablero[fila:fila + eslora, col]
+        coors_posO = tablero[fila, col: col - eslora:-1]
+    
+        # Comprobamos si esas posiciones son validas
+        # Orientacion Norte
+        if orient == 'N' and 0 <= fila - eslora < 10 and 'O' not in coors_posN:
+            tablero[fila:fila - eslora:-1, col] = 'O'
+            break
+
+        # Orientacion Este
+        elif orient == 'E' and 0 <= col + eslora < 10 and 'O' not in coors_posE:
+            tablero[fila, col: col + eslora] = 'O'
+            break
+
+        # Orientacion Sur
+        elif orient == 'S' and 0 <= fila + eslora < 10 and 'O' not in coors_posS:
+            tablero[fila:fila + eslora, col] = 'O'
+            break
+
+        # Orientacion Oeste
+        elif orient == 'O' and 0 <= col - eslora < 10 and 'O' not in coors_posO:
+            tablero[fila, col: col - eslora:-1] = 'O'
+            break
+
+        # No cumple con las dimensiones del tablero, o hay un barco ahi
+        # Probamos con otra coordenada
+        else:
+            continue
+    return ""
--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # test_HundirLaFlota
-documentos de trabajo con Toni Fortes
+
+## ⚓️ Hundir la Flota ⚓️ - Player v Machine
+
+### Índice
+
+#### Descripción del proyecto
+
+**Librerías**
+*Numpy
+
+**Definición de colocación de barcos**
+
++ Colocar los barcos mediante funciones.
++ Situa los barcos de forma aleatoria y los orienta en una dirección (Este, Oeste, Norte o Sur).
++ La selección es automática para el jugador y para la máquina.
++ No hay opción a resituar los barcos, salvo reiniciando el juego.
+
+**Sistema de turnos y disparos**
+
++ Los turnos se establecen por defecto empezando el jugador.
++ Si acierta en barco enemigo, vuelve a disparar.
++ Si falla cambiará de turno.
++ Si dispara en una ubicación ya disparada vuelve a tirar. Es una solución util para la máquina y baneable para el jugador.
++ Los disparos de la máquina son aleatorios, incluso si acierta dispará de forma aleatoria en la siente tanda.
+
+**Recursos**
+
++ No se han usado ningun recurso visual ni audiovisual.
++ Los materiales aportados están en .ipynb y .py
+
+**Documento ipynb**
+
++ Se encuentra todo el juego en un mismo documento con las liberías y listo para ir ejecuntado cada módulo.
++ Contiene varios inputs de interacción con el jugador.
+    + Inicio.
+    + Salir del juego.
+    + Introducción de las coordenadas para disparar.
+
+**Documentos py**
+
++ Un documento para cada función.
++ Un documento con la creación de mapas y sistema de turnos
+
+**Autoria**
+Jorge Fernández
+Ayuda y guía Alejandro Carábe y Alberto Romero
+
+
